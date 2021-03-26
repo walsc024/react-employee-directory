af1ce4c70b624a6ae36ff261de8ef68c2461b717
walsc024 <chriswalsh08@googlemail.com>
2021-03-26 18:36:36 +0000

    Added code for getting eomployees and setting in state (tested in console)

---
+++ src/components/Main.js
@@ -0,0 +1,12 @@
+import React from "react";
+import TableArea from "./TableArea";
+
+export default class Main extends React.Component {
+  render() {
+    return (
+      <>
+        <TableArea />
+      </>
+    );
+  }
+}
